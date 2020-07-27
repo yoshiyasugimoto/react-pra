eda51fc085ef29d061817bec94e9d5e75fb52452
yoshiyasugimoto <gabesaiti@gmail.com>
2020-07-28 07:50:36 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  // const greeting = "Hi,Tom!";
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input
+        type="text"
+        onChange={() => {
+          console.log("I am Tom!");
+        }}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
